31df173296d161ddc4817b2b6ce6d675450daf86
Thomas Daniel Galligan <thomas007g@gmail.com>
2021-07-05 19:14:08 +0100

    added mail templates for password reset

--- mail/mail.go
+++ mail/mail.go
@@ -16,11 +16,11 @@ const (
 	hostAddress = "noreply@tny.ie"
 )
 
-// SendMail takes a userAuth, subject and content to send an email
-func SendMail(user *models.UserAuth, link string) error {
+// SendMailVerification takes a userAuth, subject and content to send an email
+func SendMailVerification(user *models.UserAuth, link string) error {
 	from := mail.NewEmail(hostUser, hostAddress)
 	to := mail.NewEmail(user.Username, user.Email)
-	message := mail.NewSingleEmail(from, "Email Verification for TnyIE", to, makeTextContent(link), makeHTMLContent(link))
+	message := mail.NewSingleEmail(from, "Email Verification for TnyIE", to, makeMailVerificationTextContent(link), makeMailVerificationHTMLContent(link))
 	client := sendgrid.NewSendClient(viper.GetString("SENDGRID_CREDENTIAL"))
 
 	response, err := client.Send(message)
@@ -28,11 +28,32 @@ func SendMail(user *models.UserAuth, link string) error {
 	return err
 }
 
-func makeHTMLContent(link string) string {
-	link = "https://tny.ie/api/verify/" + link
-	return fmt.Sprintf(htmlTemplate, link, link)
+func makeMailVerificationHTMLContent(link string) string {
+	link = "https://tny.ie/api/verify/reset" + link
+	return fmt.Sprintf(htmlMailVerificationTemplate, link, link)
 }
 
-func makeTextContent(link string) string {
-	return fmt.Sprintf(textTemplate, "https://tny.ie/api/verify/"+link)
+func makeMailVerificationTextContent(link string) string {
+	return fmt.Sprintf(textMailVerificationTemplate, "https://tny.ie/api/verify/"+link)
+}
+
+func SendPasswordVerification(user *models.UserAuth, link string) error {
+	from := mail.NewEmail(hostUser, hostAddress)
+	to := mail.NewEmail(user.Username, user.Email)
+	message := mail.NewSingleEmail(from, "Password Reset for TnyIE", to, makePasswordResetTextContent(link), makePasswordResetHTMLContent(link))
+	client := sendgrid.NewSendClient(viper.GetString("SENDGRID_CREDENTIAL"))
+
+	response, err := client.Send(message)
+	log.Println(response)
+	return err
+}
+
+func makePasswordResetHTMLContent(link string) string {
+	link = "https://" + viper.GetString("tny.ui.url") + "/changepassword/" + link
+	return fmt.Sprintf(htmlPasswordVerificationTemplate, link, link)
+}
+
+func makePasswordResetTextContent(link string) string {
+	link = "https://" + viper.GetString("tny.ui.url") + "/changepassword/" + link
+	return fmt.Sprintf(textPasswordVerificationTemplate, link)
 }
--- mail/mail_tpl.go
+++ mail/mail_tpl.go
@@ -1,6 +1,6 @@
 package mail
 
-const htmlTemplate = `
+const htmlMailVerificationTemplate = `
 <!DOCTYPE html>
 <html>
 
@@ -49,7 +49,64 @@ const htmlTemplate = `
 </html>
 `
 
-const textTemplate = `
+const textMailVerificationTemplate = `
+Verification needed for TnyIE
+If you did not sign up for an account on https://tny.ie, ignore this email.
+Go to the following link to activate your account
+%s
+Terms of Service: https://ui.tny.ie/tos
+`
+
+const htmlPasswordVerificationTemplate = `
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width">
+    <style>
+        @import url('https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500;700;900&display=swap');
+
+        html,
+        body {
+            margin: 0;
+            padding: 0;
+            font-size: 14px;
+            font-weight: 400;
+            color: 'black' !important;
+            font-family: 'Roboto', sans-serif !important;
+            overflow-x: hidden;
+        }
+    </style>
+</head>
+
+<body
+    style="margin: 0;padding: 0;font-size: 14px;overflow-x: hidden;font-family: 'Roboto', sans-serif !important;">
+    <div id="body"
+        style="margin: 0;height: 100%%;width: 100%%;">
+        <header>
+        <h1>
+            Password Reset for TnyIE
+        </h1>
+        </header>
+        <p>
+            Click the link below to reset password
+        </p>
+        <div>
+            <div">
+                <a href="%s" style="color: #009688;text-decoration: none;">%s</a>
+            </div>
+        </div>
+        <p style="margin-bottom: 2em;">
+            If you did not request a password reset for <a href="https://tny.ie" style="color: #009688">https://tny.ie</a>, then ignore this email
+        </p>
+        <footer><a href="https://ui.tny.ie/tos" style="color: #009688">Terms of Service</a></footer>
+    </div>
+</body>
+</html>
+`
+
+const textPasswordVerificationTemplate = `
 Verification needed for TnyIE
 If you did not sign up for an account on https://tny.ie, ignore this email.
 Go to the following link to activate your account
